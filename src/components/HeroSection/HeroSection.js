7436f8f3a175e91200293f0d16222cf694c9b73c
Subrat kumar jena <subratj611@gmail.com>
2024-01-04 23:20:12 +0530

    feat: hero section developed

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,5 +1,41 @@
 const HeroSection = () => {
-  return <section className="h-screen bg-[#151718]"></section>;
+  return (
+    <section className="h-full bg-[#151718] text-white">
+      <div>
+        <div className="flex justify-center items-center h-screen">
+          <div className="h-[50vh] w-11/12 md:w-3/6 flex justify-center relative">
+            <div className="absolute -top-[200px] bg-gradient-to-r from-violet-700 to-green-700 w-[50vw] h-[50vh] blur-[500px] opacity-40 rounded-full"></div>
+            <div className="font-semibold text-xl text-center capitalize md:text-5xl md:leading-[4rem]">
+              <div className="text-base font-medium flex justify-center mb-3 cursor-default">
+                <div className="flex items-center bg-gray-700/50 px-5 py-1 rounded-full w-fit">
+                  <div className="h-2 w-2 bg-violet-500 mt-1 me-2 rounded-full"></div>
+                  <p className="normal-case text-base">
+                    your one stop career hub
+                  </p>
+                  <div className="words overflow-hidden"></div>
+                </div>
+              </div>
+              <div className="flex justify-center items-center">
+                <span className="px-1">Explore</span>
+                <div className="h-7 md:h-16 overflow-hidden">
+                  <span className="block text-center bg-clip-text text-transparent bg-gradient-to-r from-violet-600 to-green-300 animate-spin_words">
+                    Opportunities
+                  </span>
+                  <span className="block text-center bg-clip-text text-transparent bg-gradient-to-r from-violet-600 to-green-300 animate-spin_words">
+                    Communitites
+                  </span>
+                  <span className="block text-center bg-clip-text text-transparent bg-gradient-to-r from-violet-600 to-green-300 animate-spin_words">
+                    events & more
+                  </span>
+                </div>
+              </div>
+              <span>O.P Jindal University Training And Placement Cell</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
