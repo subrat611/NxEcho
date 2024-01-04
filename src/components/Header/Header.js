9ad3689c6da24b881fc71a0f2877809f62907073
Subrat kumar jena <subratj611@gmail.com>
2024-01-04 18:43:33 +0530

    feat: header ui develop

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import { IconBuddyGithubIcon } from "@/assets/icon";
+
+const Header = () => {
+  return (
+    <header className="absolute top-0 left-0 w-full h-max border-b border-b-gray-50/10">
+      <div className="w-full px-5 xl:mx-auto xl:w-[1128px] text-white flex items-center justify-between py-3">
+        <p className="font-semibold tracking-wider text-lg">OPJU TNP</p>
+
+        <div className="flex items-center">
+          <a href="https://google.com">
+            <IconBuddyGithubIcon classname="h-5 me-4" />
+          </a>
+          <button className="bg-violet-600 hover:bg-violet-700 px-3 py-1 rounded-md font-medium">
+            Sign in
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,5 @@
+const HeroSection = () => {
+  return <section className="h-screen bg-[#151718]"></section>;
+};
+
+export default HeroSection;
