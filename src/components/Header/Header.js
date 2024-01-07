3fbe0d39cfe06d15f3a54bd3c9bc45c7b5523f80
Subrat kumar jena <subratj611@gmail.com>
2024-01-07 13:13:44 +0530

    style: update header style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="absolute top-0 left-0 w-full h-max border-b border-b-gray-50/10">
       <div className="w-full px-5 xl:mx-auto xl:w-[1128px] text-white flex items-center justify-between py-3">
-        <p className="font-semibold tracking-wider text-lg">OPJU TNP</p>
+        <p className="font-semibold tracking-wider text-lg">NxEcho</p>
 
         <div className="flex items-center">
           <a href="https://google.com">
