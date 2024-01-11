1eca24414e2958386e76fd8f0eef0a30c260bc8c
Subrat kumar jena <subratj611@gmail.com>
2024-01-11 17:05:12 +0530

    feat: sign up page develop

---
+++ src/pages/signin.js
@@ -0,0 +1,13 @@
+import Header from "@/components/Header/Header";
+import SignInBox from "@/components/SignInBox/SignInBox";
+
+const SignIn = () => {
+  return (
+    <main className="relative h-full bg-[#151718]">
+      <Header />
+      <SignInBox />
+    </main>
+  );
+};
+
+export default SignIn;
